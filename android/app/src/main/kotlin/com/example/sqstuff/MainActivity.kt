2b91aedc710e29504627868018d7c1053f17f1e4
Ruddy <richardgryson47@gmail.com>
2023-03-31 13:05:25 +0100

    Firebase CloudFirestore

---
+++ android/app/src/main/kotlin/com/example/sqstuff/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sqstuff
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage/logic.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+import 'state.dart';
+
+class HomepageLogic extends GetxController {
+  final HomepageState state = HomepageState();
+}
